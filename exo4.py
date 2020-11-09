8449204de2569cd2c7c699bac144cd8801ebbe8c
Lihes cheriet <66468238+lihescheriet@users.noreply.github.com>
2020-11-09 12:05:59 +0100

    Create exo4.py

---
+++ exo4.py
@@ -0,0 +1,6 @@
+# Ecrire un programme en langage Python qui demande à l’utilisateur de saisir son nombre entier et de lui afficher si ce nombre est pair ou impair
+nb = int(input("Entrer un Nombre Entier"))
+if (nb % 2)== 0:
+    print("c'est pair")
+else:
+    print("c'est Impair")
